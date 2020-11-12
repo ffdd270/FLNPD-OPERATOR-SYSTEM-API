eb77f96262ad33a39b5c7c8bb96e98b4526bc4a1
ffdd270 <ffdd270@gmail.com>
2020-11-13 01:20:08 +0900

    Character Update.

--- src/routes/character.ts
+++ src/routes/character.ts
@@ -1,6 +1,8 @@
 import express, {Request} from 'express';
 import { CharacterDocuments } from "../db/documents/character";
+
 export let characterRouter = express.Router();
+
 
 async function get_character( req : Request ) : Promise<CharacterDocuments>
 {
@@ -15,6 +17,34 @@ async function get_character( req : Request ) : Promise<CharacterDocuments>
     return result;
 }
 
+type CharacterParams =
+{
+    name? : string;
+    comment? : string;
+    hp_max? : number;
+    hp? : number;
+    sp_max? : number;
+    sp? : number;
+}
+
+
+function update_attr<Type, Key extends keyof Type>( dest_data : Type, data : Type, key : Key[] )
+{
+    key.map( (key) => {
+        if ( data[key] !== undefined )
+        {
+            dest_data[key] = data[key];
+        }
+    } )
+}
+
+
+function make_character_params_from_req( req : Request ) : CharacterParams
+{
+    let params : CharacterParams = {};
+    update_attr( params, req.body, [ 'name', 'comment', 'hp_max', 'hp', 'sp_max', 'sp' ]  );
+    return params;
+}
 
 
 characterRouter.post( '/create', function( req , res, next )
@@ -38,7 +68,13 @@ characterRouter.post( '/update', async function( req , res, next )
 {
     try
     {
-        res.send( await get_character( req ) );
+        let character =  await get_character( req );
+        let param = make_character_params_from_req( req );
+
+        update_attr( character, param, [ 'name', 'comment', 'hp_max', 'hp', 'sp_max', 'sp' ] );
+        await character.save();
+
+        res.send( character );
     }
     catch (e)
     {
