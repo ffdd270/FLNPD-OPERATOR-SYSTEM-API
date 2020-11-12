3f370ddaa57f989b112debed043f1e8a554515d2
ffdd270 <ffdd270@gmail.com>
2020-11-13 01:33:10 +0900

    Impl Create.

--- src/routes/character.ts
+++ src/routes/character.ts
@@ -6,7 +6,7 @@ export let characterRouter = express.Router();
 
 async function get_character( req : Request ) : Promise<CharacterDocuments>
 {
-    if (  req.body.room_id === undefined || req.body.id === undefined ) { throw "Invalid Params."; }
+    if ( req.body.room_id === undefined || req.body.id === undefined ) { throw "Invalid Params."; }
 
     let room_id : string = req.body.room_id;
     let id : string = req.body.id;
@@ -15,6 +15,28 @@ async function get_character( req : Request ) : Promise<CharacterDocuments>
 
     if ( result == null ) { throw "Not Found"; }
     return result;
+}
+
+async function find_character( req : Request ) : Promise<boolean>
+{
+    if ( req.body.room_id === undefined || req.body.id === undefined ) { throw "Invalid Params."; }
+
+    let room_id : string = req.body.room_id;
+    let id : string = req.body.id;
+
+    return await CharacterDocuments.findOne( { where: { id: id, room_id: room_id } }) !== null;
+}
+
+type CharacterCreateParams =
+{
+    id : string;
+    room_id : string;
+    name? : string;
+    comment? : string;
+    hp_max? : number;
+    hp? : number;
+    sp_max? : number;
+    sp? : number;
 }
 
 type CharacterParams =
@@ -47,9 +69,27 @@ function make_character_params_from_req( req : Request ) : CharacterParams
 }
 
 
-characterRouter.post( '/create', function( req , res, next )
+characterRouter.post( '/create',  async function( req , res, next )
 {
-    res.send("character add");
+    try
+    {
+        if ( await find_character(req) )
+        {
+            throw  "ALREADY CHARACTER HERE.";
+        }
+
+        let create_params : CharacterCreateParams = { id: req.body.id, room_id: req.body.room_id };
+        let params = make_character_params_from_req( req );
+        update_attr( create_params, params, [ 'name', 'comment', 'hp_max', 'hp', 'sp_max', 'sp' ] );
+
+        let new_char = await CharacterDocuments.create( create_params );
+        res.send( new_char );
+    }
+    catch (e)
+    {
+        res.statusCode = 422;
+        res.send(e);
+    }
 });
 
 characterRouter.get( '/get', async function( req : Request, res, next )
@@ -60,6 +100,7 @@ characterRouter.get( '/get', async function( req : Request, res, next )
     }
     catch (e)
     {
+        res.statusCode = 422;
         res.send(e);
     }
 });
@@ -78,6 +119,7 @@ characterRouter.post( '/update', async function( req , res, next )
     }
     catch (e)
     {
+        res.statusCode = 422;
         res.send(e);
     }
 });
@@ -89,10 +131,13 @@ characterRouter.post( '/delete', async function( req , res, next )
     {
         let character =  await get_character( req );
         await character.destroy();
+
+        res.statusCode = 200;
         res.send("character delete");
     }
     catch (e)
     {
+        res.statusCode = 422;
         res.send(e);
     }
 });
