ec7acad8abbf8156e8c16d5a4488fdc5e43658e1
ffdd270 <ffdd270@gmail.com>
2020-11-12 23:02:32 +0900

    Impl Get.

--- src/routes/character.ts
+++ src/routes/character.ts
@@ -9,10 +9,10 @@ characterRouter.post( '/create', function( req , res, next )
     res.send("character add");
 });
 
-characterRouter.get( '/get/:room_id/:id', async function( req , res, next )
+characterRouter.get( '/get', async function( req , res, next )
 {
-    let room_id : string = req.params.room_id;
-    let id : string = req.params.id;
+    let room_id : string = req.body.room_id;
+    let id : string = req.body.id;
 
     let result  = await CharacterDocuments.findOne( { where: { id: id, room_id: room_id } })
     if( result == null )
